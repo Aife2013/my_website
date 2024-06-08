80c9079ac71ff0dcb2d9a783b0685600438cd446
Maansi <maansi0613@gmail.com>
2024-06-08 16:41:45 +0530

    Update and rename script.js to cv.html

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.getElementById('dark-mode-toggle');
-    toggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
-    });
-});
